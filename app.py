43e2a24a9e7bf90c00b2b141981098d7758c63bb
Ethan Lowder <elowder@iwu.edu>
2024-02-12 10:15:16 -0600

    changed get errors and variable name errors

--- app.py
+++ app.py
@@ -3,16 +3,22 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def index():  # put application's code here
     return render_template("index.html")
 
 
-@app.route("/solution", methods=["POST", "GET"])
+@app.route("/solution")
 def wordCount():
-    num1 = int(request.args.get("num1"))
+    try:
+        num1 = int(request.args.get("num1"))
+    except ValueError:
+        num1 = 0
+    try:
+        num2 = int(request.args.get("num2"))
+    except ValueError:
+        num2 = 0
     operation = request.args.get("operation")
-    num2 = int(request.args.get("num2"))
 
     if operation == "+":
         result = num1 + num2
@@ -25,6 +31,7 @@ def wordCount():
             result = num1 / num2
         except ZeroDivisionError:
             return render_template("failure.html")
+
     return render_template("solutions.html", result=result, num1=num1, operation=operation, num2=num2)
 
 
